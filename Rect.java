456bf82dd90d6d55eda4dc8216d69db8f0a69287
lullulalla <jisoo950225@gmail.com>
2024-03-19 10:17:34 +0900

    setter getter 추가

--- Rect.java
+++ Rect.java
@@ -4,4 +4,20 @@ class Rect{
 	public int getArea(){
 		return width * height;
 	}
+	public void sayHello(){
+      		System.out.println("Hello," + name);
+  	 }
+  	public void setWidth(int width){
+  		this.width = width;
+ 	 }
+ 	 public int getWidth(int width){
+  		return name;
+ 	 }
+ 	 public void setHeight(int height){
+  		this.height = height;
+ 	 }
+  	public int getHeight(int height){
+  		return height;
+	  }
+
 }
--- person.java
+++ person.java
@@ -4,4 +4,16 @@ class Person{
    public void sayHello(){
       System.out.println("Hello," + name);
    }
+  public void setName(String name){
+  	this.name = name;
+  }
+  public String getName(String name){
+  	return name;
+  }
+  public void setAge(int age){
+  	this.age = age;
+  }
+  public int getAge(int age){
+  	return age;
+  }
 }
